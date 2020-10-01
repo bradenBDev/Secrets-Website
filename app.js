46b7262d94428d980d2e20eea203ba6ec5df9099
Braden Baird <bradenbaird@Bradens-MacBook-Pro.local>
2020-10-01 11:31:23 -0600

    Change OAuth to work with Heroku app

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL: "https://tranquil-wildwood-54459.herokuapp.com/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refreshToken, profile, cb) {
@@ -76,7 +76,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "http://localhost:3000/auth/facebook/secrets"
+        callbackURL: "https://tranquil-wildwood-54459.herokuapp.com/auth/facebook/secrets"
     },
     function(accessToken, refreshToken, profile, cb) {
         User.findOrCreate({ facebookId: profile.id, googleId: null }, function (err, user) {
